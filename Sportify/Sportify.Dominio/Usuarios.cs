ac7794907e3ead94817d57fa61ac523b2ea689cb
Iann <145216214+GeekTaps@users.noreply.github.com>
2026-05-12 17:10:00 -0300

    Base Del Proycto - Iann

---
+++ Sportify/Sportify.Dominio/Usuarios.cs
@@ -0,0 +1,9 @@
+namespace Sportify.Dominio;
+
+public class Usuario{
+    private int Id { get; set; }
+    private string DNI { get; set; }
+    private string Nombre { get; set; }
+    private string Email { get; set; }
+    private string Contraseña{ get; set; }
+}
