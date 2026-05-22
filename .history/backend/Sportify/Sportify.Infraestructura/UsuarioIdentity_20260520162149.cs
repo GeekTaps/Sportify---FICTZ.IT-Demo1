f3554948c7b05cddead45af4b259079569ec391f
zega84 <joaki.zg@hotmail.com>
2026-05-22 16:41:07 -0300

    elimino archivos innecesarios

--- .history/backend/Sportify/Sportify.Infraestructura/UsuarioIdentity_20260520162149.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Sportify.Infraestructura.Identity;
-
-public class UsuarioIdentity : IdentityUser
-{
-    public string NombreCompleto { get; set; } = "";
-}
